eeeea68b35d85e55131c727540b0a9c757c4b899
Vladislav Zubakin <shaqimusic@gmail.com>
2022-08-25 03:07:01 +0300

    feat: add binance endpoints

---
+++ src/api/BinanceEndpoints.ts
@@ -0,0 +1,5 @@
+export enum BinanceEndpoints {
+  KLINES = 'https://api.binance.com/api/v3/klines',
+  EXCHANGE_INFO = 'https://api.binance.com/api/v3/exchangeInfo',
+  TICKER_STATS_24H = 'https://api.binance.com/api/v3/ticker/24hr',
+}

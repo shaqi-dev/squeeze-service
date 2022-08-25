6b2326300be6a334822daad6e178a9f21cdba928
Vladislav Zubakin <shaqimusic@gmail.com>
2022-08-25 18:35:34 +0300

    feat: add volume filter

--- src/components/SymbolsSettings/SymbolsSettings.tsx
+++ src/components/SymbolsSettings/SymbolsSettings.tsx
@@ -1,9 +1,11 @@
-import { FC, useState } from 'react'
+import { FC, useState, useRef } from 'react'
 import { useLazyGetAllSymbolsStatsQuery } from '../../api/symbolStatsApi'
 import SymbolsTable from '../SymbolsTable'
 
 const SymbolsSettings: FC = () => {
   const [symbolsStats, setSymbolsStats] = useState<{ symbol: string; volume: string }[]>([])
+  const [minVolumeFilter, setMinVolumeFilter] = useState(false)
+  const minVolumeRef = useRef<HTMLInputElement>(null)
   const [getAllSymbolsStats] = useLazyGetAllSymbolsStatsQuery()
 
   const handleUpdateVolumes = async (): Promise<void> => {
@@ -15,7 +17,18 @@ const SymbolsSettings: FC = () => {
           symbol: symbol.symbol,
           volume: symbol.quoteVolume,
         }))
-        .filter((symbol) => symbol.symbol.match(/\b\w+USDT\b/g))
+        .filter((symbol) => {
+          const RE_USDT = /\b\w+USDT\b/g
+          let res = !!symbol.symbol.match(RE_USDT)
+
+          if (res && minVolumeFilter && minVolumeRef.current) {
+            const convertedVolume = parseInt(symbol.volume, 10) / 1000000
+            const currentMinVolume = +minVolumeRef.current.value
+            res = convertedVolume >= currentMinVolume
+          }
+
+          return res
+        })
         .sort((a, b) => +b.volume - +a.volume)
 
       setSymbolsStats(result)
@@ -25,7 +38,21 @@ const SymbolsSettings: FC = () => {
   return (
     <div className="flex flex-col items-center gap-3">
       <SymbolsTable symbols={symbolsStats} />
-      <div className="flex gap-3">
+      <div className="flex flex-col gap-3">
+        <label htmlFor="min-vol" className="flex items-center gap-2 text-base">
+          <input
+            type="checkbox"
+            checked={minVolumeFilter}
+            onChange={(): void => setMinVolumeFilter(!minVolumeFilter)}
+          />
+          Minimal volume, m.:
+          <input
+            type="text"
+            className="w-20 rounded-md border border-neutral-900 py-2 px-4 text-base disabled:border-neutral-400 disabled:bg-gray-200 disabled:text-gray-500"
+            ref={minVolumeRef}
+            disabled={!minVolumeFilter}
+          />
+        </label>
         <button
           type="button"
           className="rounded-md bg-neutral-900 py-2 px-4 text-base font-semibold text-white transition-all duration-100 hover:bg-neutral-800"

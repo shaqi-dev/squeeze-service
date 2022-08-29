3f5c1f8062fafa022cbbc87d034a8f9d2daab77d
Vladislav Zubakin <shaqimusic@gmail.com>
2022-08-29 05:20:13 +0300

    feat: add react hook form

--- src/components/ConfigsSettings/ConfigsSettings.tsx
+++ src/components/ConfigsSettings/ConfigsSettings.tsx
@@ -1,88 +1,68 @@
-import { FC, useState, useRef, FormEventHandler } from 'react'
+import { FC, useState, useEffect, useCallback } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 import InputNumber from '../InputNumber'
 import { countSettings } from '../../utils/countSettings'
 import { createConfigs } from '../../utils/createConfigs'
 
-const ConfigsSettings: FC = () => {
-  const inputWidth = 'w-24'
-  const inputStep = 0.1
-  const gridItemStyle = 'border border-neutral-900 py-1 px-3 text-center'
+export interface ConfigsSettingsForm {
+  buyFrom: number
+  buyTo: number
+  buyStep: number
+  sellFrom: number
+  sellTo: number
+  sellStep: number
+  stopFrom: number
+  stopTo: number
+  stopStep: number
+}
 
+const ConfigsSettings: FC = () => {
   const [configsCount, setConfigsCount] = useState(0)
 
-  const buyFrom = useRef<HTMLInputElement>(null)
-  const buyTo = useRef<HTMLInputElement>(null)
-  const buyStep = useRef<HTMLInputElement>(null)
-  const sellFrom = useRef<HTMLInputElement>(null)
-  const sellTo = useRef<HTMLInputElement>(null)
-  const sellStep = useRef<HTMLInputElement>(null)
-  const stopFrom = useRef<HTMLInputElement>(null)
-  const stopTo = useRef<HTMLInputElement>(null)
-  const stopStep = useRef<HTMLInputElement>(null)
+  const { register, watch, handleSubmit } = useForm<ConfigsSettingsForm>()
+  const watchInputs = watch()
 
-  const settings = {
-    buy: {
-      from: Number(buyFrom.current?.value),
-      to: Number(buyTo.current?.value),
-      step: Number(buyStep.current?.value),
-    },
-    sell: {
-      from: Number(sellFrom.current?.value),
-      to: Number(sellTo.current?.value),
-      step: Number(sellStep.current?.value),
-    },
-    stop: {
-      from: Number(stopFrom.current?.value),
-      to: Number(stopTo.current?.value),
-      step: Number(stopStep.current?.value),
-    },
-  }
+  const updateConfigsCount = useCallback((): void => {
+    const { buyFrom, buyTo, buyStep, sellFrom, sellTo, sellStep, stopFrom, stopTo, stopStep } =
+      watchInputs
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e): void => {
-    e.preventDefault()
+    const buySettingsCount = countSettings(+buyFrom, +buyTo, +buyStep)
+    const sellSettingsCount = countSettings(+sellFrom, +sellTo, +sellStep)
+    const stopSettingsCount = countSettings(+stopFrom, +stopTo, +stopStep)
+
+    const configsTotalCount = buySettingsCount * sellSettingsCount * stopSettingsCount
+
+    if (configsTotalCount > 0) {
+      setConfigsCount(configsTotalCount)
+    } else {
+      setConfigsCount(0)
+    }
+  }, [watchInputs])
+
+  useEffect(() => {
+    updateConfigsCount()
+  }, [watch, updateConfigsCount])
+
+  const onSubmit: SubmitHandler<ConfigsSettingsForm> = (data): void => {
+    const { buyFrom, buyTo, buyStep, sellFrom, sellTo, sellStep, stopFrom, stopTo, stopStep } = data
+
+    const settings = {
+      buy: { from: buyFrom, to: buyTo, step: buyStep },
+      sell: { from: sellFrom, to: sellTo, step: sellStep },
+      stop: { from: stopFrom, to: stopTo, step: stopStep },
+    }
+
     createConfigs(settings)
   }
 
-  const handleChangeForm = (): void => {
-    if (
-      buyFrom.current &&
-      buyTo.current &&
-      buyStep.current &&
-      sellFrom.current &&
-      sellTo.current &&
-      sellStep.current &&
-      stopFrom.current &&
-      stopTo.current &&
-      stopStep.current
-    ) {
-      const buySettingsCount = countSettings(
-        +buyFrom.current.value,
-        +buyTo.current.value,
-        +buyStep.current.value,
-      )
-      const sellSettingsCount = countSettings(
-        +sellFrom.current.value,
-        +sellTo.current.value,
-        +sellStep.current.value,
-      )
-      const stopSettingsCount = countSettings(
-        +stopFrom.current.value,
-        +stopTo.current.value,
-        +stopStep.current.value,
-      )
-
-      const configsTotalCount = buySettingsCount * sellSettingsCount * stopSettingsCount
-
-      if (configsTotalCount > 0) {
-        setConfigsCount(configsTotalCount)
-      } else {
-        setConfigsCount(0)
-      }
-    }
-  }
+  const inputWidth = 'w-24'
+  const inputStep = 0.1
+  const gridItemStyle = 'border border-neutral-900 py-1 px-3 text-center'
+  const minPercent = 0.5
+  const minStep = 0.1
 
   return (
-    <form className="flex flex-col gap-2" onSubmit={handleSubmit} onChange={handleChangeForm}>
+    <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
       <h3 className="text-center text-xl font-bold">Configs Settings</h3>
 
       <div className="grid h-fit grid-cols-4 text-base">
@@ -93,71 +73,71 @@ const ConfigsSettings: FC = () => {
 
         <span className={`${gridItemStyle} bg-blue-100 font-medium`}>Buy</span>
         <InputNumber
-          ref={buyFrom}
+          {...register('buyFrom', { required: true, min: minPercent })}
           className={`${inputWidth} ${gridItemStyle} bg-blue-100`}
           step={inputStep}
           defaultValue={3}
-          min={0}
+          min={minPercent}
         />
         <InputNumber
-          ref={buyTo}
+          {...register('buyTo', { required: true, min: minPercent })}
           className={`${inputWidth} ${gridItemStyle} bg-blue-100`}
           step={inputStep}
           defaultValue={5}
-          min={0}
+          min={minPercent}
         />
         <InputNumber
-          ref={buyStep}
+          {...register('buyStep', { required: true, min: minStep })}
           className={`${inputWidth} ${gridItemStyle} bg-blue-100`}
           step={inputStep}
           defaultValue={0.2}
-          min={0}
+          min={minStep}
         />
 
         <span className={`${gridItemStyle} bg-purple-100 font-medium`}>Sell</span>
         <InputNumber
-          ref={sellFrom}
+          {...register('sellFrom', { required: true, min: minPercent })}
           className={`${inputWidth} ${gridItemStyle} bg-purple-100`}
           step={inputStep}
           defaultValue={1}
-          min={0}
+          min={minPercent}
         />
         <InputNumber
-          ref={sellTo}
+          {...register('sellTo', { required: true, min: minPercent })}
           className={`${inputWidth} ${gridItemStyle} bg-purple-100`}
           step={inputStep}
           defaultValue={2.5}
-          min={0}
+          min={minPercent}
         />
         <InputNumber
-          ref={sellStep}
+          {...register('sellStep', { required: true, min: minStep })}
           className={`${inputWidth} ${gridItemStyle} bg-purple-100`}
           step={inputStep}
           defaultValue={0.2}
-          min={0}
+          min={minStep}
         />
 
         <span className={`${gridItemStyle} bg-red-100 font-medium`}>Stop</span>
         <InputNumber
-          ref={stopFrom}
+          {...register('stopFrom', { required: true, min: minPercent })}
           className={`${inputWidth} ${gridItemStyle} bg-red-100`}
           step={inputStep}
           defaultValue={2}
-          min={0}
+          min={minPercent}
         />
         <InputNumber
-          ref={stopTo}
+          {...register('stopTo', { required: true, min: minPercent })}
           className={`${inputWidth} ${gridItemStyle} bg-red-100`}
           step={inputStep}
           defaultValue={3.5}
-          min={0}
+          min={minPercent}
         />
         <InputNumber
-          ref={stopStep}
+          {...register('stopStep', { required: true, min: minStep })}
           className={`${inputWidth} ${gridItemStyle} bg-red-100`}
           step={inputStep}
           defaultValue={0.2}
-          min={0}
+          min={minStep}
         />
       </div>
       <p className="text-center text-base text-gray-400">Total configs: {configsCount}</p>
